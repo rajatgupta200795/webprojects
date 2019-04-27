5332f74f4566148ca8446e38c5d67722991e97ac
Rajat Gupta <rajatgupta200795@gmail.com>
2019-04-27 23:16:47 +0530

    Update connect.php

--- school/inc/connect.php
+++ school/inc/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = new mysqli('localhost','root','Rajat@20071995','mcskktco_school') or die("could not connect to mysql".mysqli_error($con));
+$con = new mysqli('localhost','root','Rajat@20071995','mcskktco_school.sql') or die("could not connect to mysql".mysqli_error($con));
 
 
 ?>
