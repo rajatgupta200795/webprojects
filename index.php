4d0bc0476e5631391c50bd0d37eafd1998a8bead
Rajat Gupta <rajatgupta200795@gmail.com>
2019-04-27 22:05:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -76,7 +76,22 @@ To create your wishes, enter your name and receiver\'s name, generate link and s
 
 echo'
 </br>
-<b>My Projects</b>
+<div class="container-fluid">
+<div class="row" style="font-size:15px;"><div class="col-sm-1"><span style="font-size:40px; color:#128ea2;" class="glyphicon glyphicon-fullscreen"></span>
+</div>
+<div class="col-sm-10"><span style="font-size:25px; font-family:impact;"><a style="color:#635656;" href="/my-wishes">A Dark Night - Animation Story</a></span>
+</br>
+A dark night animation story with horror scenes.</br>
+An animation programmed in html css and javascript with a horror thunder sound.
+</div>
+</div>
+</div>
+';
+
+
+echo'
+</br>
+<b>Blog and Entertainment</b>
 <hr>';
 
 
