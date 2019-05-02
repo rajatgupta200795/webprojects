90167f7e446aa712da428215624f482e2bc42b5e
Rashmi Gupta <50044838+rashmi167@users.noreply.github.com>
2019-05-03 01:03:34 +0530

    Update header.php

--- my-wishes/inc/header.php
+++ my-wishes/inc/header.php
@@ -4,12 +4,12 @@
     <meta name="description" content="Send online wishes to your friends, girlfriend, relative by sendting them your ewishes.">
 
 <meta name="viewport" content="width=400">
-<link rel="icon" href="http://www.ewishes.online/img/title_pic.png">
+<link rel="icon" href="http://www.devopsrider.com/img/title_pic.png">
 
 
-<link rel="stylesheet" href="http://www.ewishes.online/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://www.devopsrider.com/css/bootstrap.min.css">
 
-<link href="http://www.ewishes.online/css/google_font_code.css?family=Josefin+Slab|Hind|Raleway|Poppins|Pangolin|Source+Sans+Pro|PT+Sans+Caption|Oswald|PT+Sans" rel="stylesheet">
+<link href="http://www.devopsrider.com/css/google_font_code.css?family=Josefin+Slab|Hind|Raleway|Poppins|Pangolin|Source+Sans+Pro|PT+Sans+Caption|Oswald|PT+Sans" rel="stylesheet">
 
 <script src="../js/angular.min.js" ></script>
 
@@ -38,16 +38,16 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="http://www.ewishes.online/" style="font-size: 30px;">ewishes.online</a>
+      <a class="navbar-brand" href="http://www.devopsrider.com/" style="font-size: 30px;">ewishes.online</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
 
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="http://www.ewishes.online/">Home</a></li>
-        <li><a href="http://www.ewishes.online/#ewishes">Wishes</a></li>
-        <li><a href="http://www.ewishes.online/#wishSong">Songs</a></li>
-        <li><a href="http://www.ewishes.online/about-us.php">About</a></li>
-        <li><a href="http://www.ewishes.online/contact-us.php">Contact</a></li>
+        <li><a href="http://www.devopsrider.com/">Home</a></li>
+        <li><a href="http://www.devopsrider.com/#ewishes">Wishes</a></li>
+        <li><a href="http://www.devopsrider.com/#wishSong">Songs</a></li>
+        <li><a href="http://www.devopsrider.com/about-us.php">About</a></li>
+        <li><a href="http://www.devopsrider.com/contact-us.php">Contact</a></li>
       </ul>
     </div>
   </div>
