660d3147a460f9b7daeb4cd9bed08136fcc35f38
Rashmi Gupta <50044838+rashmi167@users.noreply.github.com>
2019-05-03 02:32:18 +0530

    Update contact-us.php

--- my-wishes/contact-us.php
+++ my-wishes/contact-us.php
@@ -26,9 +26,9 @@
 <hr style="width: 100%; text-align: center; height: 1px; border-top: 1px solid #f3f3ee; background-color: #f7f7f7;">
 </br>
 
-<p style="font-family: 'Josefin Slab', serif; font-size: 25px; font-weight: bold;">Website Owner ?</p>
+<p style="font-family: 'Josefin Slab', serif; font-size: 25px; font-weight: bold;">Website Owner </p>
 <p style="font-family: 'PT Sans Caption', sans-serif; font-size: 18px;">
-Thank you for your intrest in <a href="http://www.ewishes.online/">ewishes.online</a>. We are happy to find ourselves with you in your happy moment.
+Thank you for your intrest in <a href="http://www.devopsrider.com/">Digital Ewishes</a>. We are happy to find ourselves with you in your happy moment.
 </br></br>
 If you have any issues, suggestion or need of any kind of help, Please feel free to contact us.
 </br></br>
@@ -48,7 +48,7 @@ Rajat Gupta is also intrested in any other web development activity. So if you w
 </br></br></br></br>
 
 <b>Thank You</br>
-ewishes.online Team
+Digital Ewishes Team
 </b>
 </p>
 </p>
