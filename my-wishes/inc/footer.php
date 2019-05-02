2d72426974861b16446265ccfd2a7570d7160750
Rashmi Gupta <50044838+rashmi167@users.noreply.github.com>
2019-05-03 01:09:54 +0530

    Update footer.php

--- my-wishes/inc/footer.php
+++ my-wishes/inc/footer.php
@@ -12,7 +12,7 @@
 <div style="padding-right:0px; position: absolute; width: 110%; text-align: center; height: <?php if(!isMobileDevice()) echo'65%'; else echo'120%'; ?>; border-top: 1px solid #f3f3ee; background-color: #f0f0f0;">
     <div class="row">
         <div class="col-sm-3">
-            <div style="font-family: 'Josefin Slab', serif; font-size: 35px; padding: 25px; color: ; font-weight: bold;"><a href="http://www.ewishes.online/"  style="color: black;"">ewishes.online</a></div>
+            <div style="font-family: 'Josefin Slab', serif; font-size: 35px; padding: 25px; color: ; font-weight: bold;"><a href="http://www.devopsrider.com/"  style="color: black;"">Digital Wishes</a></div>
         </div>
         <div class="col-sm-8" style="color: ; padding: 20px; font-size: 15px;font-family: 'PT Sans Caption', sans-serif;"> 
 
@@ -23,21 +23,21 @@
     <div class="col-sm-4" style="font-size: 17px; text-align:left;">
         <b style="font-size: 20px; ">Festivals</b>
         <ul style="font-family: 'PT Sans', sans-serif; font-weight: normal;">
-            <li><a  href="http://www.ewishes.online/create-ewishes-for-diwali/index.php">Diwali Wishes.</a></li>
-            <li><a href="http://www.ewishes.online/create-ewishes-for-dhanteras">Dhanteras Wishes</a></li>
-            <li><a href="http://www.ewishes.online/create-ewishes-for-narak-chaudas">Narak Chaudas Wishes</a></li>
-            <li><a href="http://www.ewishes.online/create-ewishes-for-bhaiya-dooj">Bhai Dooj Wishes</a></li>
-            <li><a href="http://www.ewishes.online/create-ewishes-for-guru-purnima">Guru Purnima Wishes</a></li>
-            <li><a href="http://www.ewishes.online/create-ewishes-for-dussehra">Dussehra Wishes.</a></li>
+            <li><a  href="http://www.devopsrider.com/create-ewishes-for-diwali/index.php">Diwali Wishes.</a></li>
+            <li><a href="http://www.devopsrider.com/create-ewishes-for-dhanteras">Dhanteras Wishes</a></li>
+            <li><a href="http://www.devopsrider.com/create-ewishes-for-narak-chaudas">Narak Chaudas Wishes</a></li>
+            <li><a href="http://www.devopsrider.com/create-ewishes-for-bhaiya-dooj">Bhai Dooj Wishes</a></li>
+            <li><a href="http://www.devopsrider.com/create-ewishes-for-guru-purnima">Guru Purnima Wishes</a></li>
+            <li><a href="http://www.devopsrider.com/create-ewishes-for-dussehra">Dussehra Wishes.</a></li>
             
         </ul>
     </div>
     <div class="col-sm-4" style="font-size: 17px;  text-align:left;">
         <b  style="font-size: 20px; ">Special Occasions</b>
         <ul style="font-family: 'PT Sans', sans-serif; font-weight: normal;">
-            <li><a href="http://www.ewishes.online/create-ewishes-for-birth-day">Birthday Wishes.</a></li>
-            <li><a href="http://www.ewishes.online/create-ewishes-for-new-year">New Year Wishes.</a></li>
-            <li><a href="http://www.ewishes.online/create-ewishes-for-marriage-anniversary">Marriage Anniversary Wishes.</a></li>
+            <li><a href="http://www.devopsrider.com/create-ewishes-for-birth-day">Birthday Wishes.</a></li>
+            <li><a href="http://www.devopsrider.com/create-ewishes-for-new-year">New Year Wishes.</a></li>
+            <li><a href="http://www.devopsrider.com/create-ewishes-for-marriage-anniversary">Marriage Anniversary Wishes.</a></li>
         </ul>
         </br>
         
@@ -45,14 +45,14 @@
     <div class="col-sm-3" style="font-size: 17px;  text-align:left;">
         <b  style="font-size: 20px; ">Best Of Luck</b>
         <ul style="font-family: 'PT Sans', sans-serif; font-weight: normal;">
-            <li><a href="http://www.ewishes.online/create-best-of-luck-ewishes-for-job-interview">For Job Interview.</a></li>
+            <li><a href="http://www.devopsrider.com/create-best-of-luck-ewishes-for-job-interview">For Job Interview.</a></li>
         </ul>
     </div>
     </div>
     
 </br></br>        
         
-            <a href="http://www.ewishes.online/privacypolicy.php" style="color: black;">Privacy Policy</a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; <a href="http://www.ewishes.online/about-us.php" style="color: black;">About Us</a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; <a href="http://www.ewishes.online/contact-us.php" style="color: black;">Contact Us</a>
+            <a href="http://www.devopsrider.com/privacypolicy.php" style="color: black;">Privacy Policy</a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; <a href="http://www.devopsrider.com/about-us.php" style="color: black;">About Us</a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; <a href="http://www.devopsrider.com/contact-us.php" style="color: black;">Contact Us</a>
             </br></br>
             All Rights Reserved | Copyright 2017</br>
             Powered by RGroup</br>
