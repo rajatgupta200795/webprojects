efd9cd2c103ba2bbf2421f557eb6c6bd81973dfd
Rajat Gupta <rajatgupta200795@gmail.com>
2019-05-03 22:54:17 +0530

    Update footer.php

--- homeecart/inc/footer.php
+++ homeecart/inc/footer.php
@@ -1,7 +1,7 @@
 
 </br></br></br></br></br></br>
 
-<div style="width: 100%; height: 30%; background-color: #1f1e1e; position: absolute; "></div> 
+<div style="width: 100%; height: 40%; background-color: #1f1e1e; position: absolute; "></div> 
 
 </body>
 </html>
