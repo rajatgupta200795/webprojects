e2ef95408e41acf314b3af73e88fa404f1eefbea
Rajat Gupta <rajatgupta200795@gmail.com>
2019-05-03 23:28:46 +0530

    Update header.php

--- bharatbazar/inc/header.php
+++ bharatbazar/inc/header.php
@@ -20,7 +20,7 @@ $query=$con->query("SELECT * FROM buy_req WHERE  1=1");
 <head>
 	<title>BharatBazar - Indian Exporters, Manufacturers, Suppliers Directory</title>
 
-	<link rel="shortcut icon" href="http://www.ewishes.online/title.png" type="image/png"/>
+	<link rel="shortcut icon" href="../title.png" type="image/png"/>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
