072437c2fff8f6b6985dc1ebc5538fff548e683f
Rashmi Gupta <50044838+rashmi167@users.noreply.github.com>
2019-05-04 18:26:18 +0530

    Update footer.php

--- school/inc/footer.php
+++ school/inc/footer.php
@@ -10,9 +10,9 @@
 
 <div style="padding-top:6%; text-align:center; width:100%;font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; padding-bottom:8%; background-color:#2c2b2b; text-align:center; color:white;">
 
-	&copy; Copyright 2017 | www.mcskkt.com</br>
+	&nbsp;&nbsp;&nbsp;&copy; Copyright 2017 </br>
 	All Rights Reserved<br>
-	RGrouP Production    |    Contact Us +919807264017  |    Mail Us @ rajatgupta200795@gmail.com
+	RGrouP Production    |    Contact Us +917081717420  |    Mail Us @ rajatgupta200795@gmail.com
 
 </div>
 
