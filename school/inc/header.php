dc5f7b95c48477ff940152910d36f04b7fd0602d
Rashmi Gupta <50044838+rashmi167@users.noreply.github.com>
2019-05-04 18:27:08 +0530

    Update header.php

--- school/inc/header.php
+++ school/inc/header.php
@@ -202,7 +202,7 @@ a:link
   js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-<a class="navbar-brand" href="http://www.devopsrider.com/" style="font-size: 30px; color: white; padding-top:37px;margin-left- 10px; "> <b> Home </b></a>
+<a class="navbar-brand" href="http://www.devopsrider.com/" style="font-size: 30px; color: white; padding-top:37px;padding-left- 10px; "> <b> Home </b></a>
 
 
 <?php if(1==1){
