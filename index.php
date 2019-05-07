e7ef9d41e35f52650e8bb24ad2bb0159ac5c40a4
Rashmi Gupta <50044838+rashmi167@users.noreply.github.com>
2019-05-08 01:26:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ echo'
 <div class="container-fluid">
 <div class="row" style="font-size:15px;"><div class="col-sm-1"><span style="font-size:50px; color:black;" class="glyphicon glyphicon-shopping-cart"></span>
 </div>
-<div class="col-sm-10"><span style="font-size:25px; font-family:impact;"><a style="color:#635656;" href="/ewishes/">Ewishes - Online Marketplace</a></span>
+<div class="col-sm-10"><span style="font-size:25px; font-family:impact;"><a style="color:#359487;" href="/ewishes/">Ewishes - Online Marketplace</a></span>
 </br>
 An online marketplace is a type of e-commerce site where product or service information is provided  by multiple third parties, whereas transactions are processed by the marketplace operator.
 <!--In today\'s busy lifestyle, pure and genuine items have become out of reach</br> and shopkeepers are evading taxes  and becoming rich.</br>At Bharat Bazaar we provide branded articles for daily use house hold items,</br> Kitchen Accessories, Grocery, Apparel all under one roof with bill.
